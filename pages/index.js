2dc21348d88f63ebf9464ff9873ea778230ec044
htorken <htorken@gmail.com>
2026-04-15 01:10:02 -0300

    Update files

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>Teste de Tabnews do Michel TIeló (Filipe Deschamps)</h1>
+    return<h2> Stephaninha, estou trabalhando no meu projeto com curso.dev!</h2>
+}
+
+export default Home;
